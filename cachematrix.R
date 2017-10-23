c49af10c1652ae48456522321510565f84795bf6
RWLIII <rick.w.lilley@gmail.com>
2017-10-22 20:05:36 -0700

    Complete code for homework assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+## NAME: Rick Lilley
+## Assignment: R Programming Assingnment #2: 
+## Description: 
+## Create and write a pair of functions that cache
+## the inverse of a function
+## ----------------------------------------------------
 
-## Write a short comment describing this function
+## Description:
+## The makeCacheMatrix function creates a matrix object
+## that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ##Initialize variable to store inverse
+  inverseMat <- NULL
+  
+  ##Set function 
+  set <- function(y){
+    x <<- y
+    inverseMat <<- NULL
+  }
+  
+  ##get function
+  get <- function() {x}
+  
+  ##setInverse function
+  setInverse <- function(inv) {inverseMat <<- inv}
+  
+  ##getInverse function
+  getInverse <- function() {inverseMat}
+  
+  ##list of all functions
+  list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
+  
 }
+##--------------------------------------------------
+
+## Description:
+## This Function computes the inverse fo the matrix 
+## returned by cacheMatrix. If the inverse has been
+## solved, it returns the inverse from the cache
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x = matrix(), ...) {
+  ##initialize storage matrix
+  inv <- x$getInverse()
+  
+  ##check to see if the inverse is in cache
+  if(!is.null(inv)) {
+    message("Retrieving cached matrix")
+    return(inv)
+  }
+  
+  ##if the inverse does not exist, solve for it
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setInverse(inv)
+  
+  ## Return a matrix that is the inverse of 'x'
+  inv
 }
+##---------------------------------------------------
